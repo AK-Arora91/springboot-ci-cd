ee22c45d5274470dc70b6cdb6d5bd0c82df0d616
Ankit Arora <ankarora@teksystems.com>
2021-05-03 23:41:43 +0530

    Initial Commit

---
+++ springboot-ci-cd/src/main/java/org/springboot/springbootcicd/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.springboot.springbootcicd;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootCiCdApplication.class);
+	}
+
+}
---
+++ springboot-ci-cd/src/main/java/org/springboot/springbootcicd/SpringbootCiCdApplication.java
@@ -0,0 +1,13 @@
+package org.springboot.springbootcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCiCdApplication.class, args);
+	}
+
+}
